244a1ec0c56ac6b4c657a77b4c1d26fe2f2c2046
Nuno Jesus <up201905477@edu.fe.up.pt>
2022-03-29 02:26:29 +0100

    Corrected exercise 4.

--- f3/ex4.c
+++ f3/ex4.c
@@ -5,25 +5,45 @@
 #define READ_ONLY 0
 #define LOWERCASE 1
 #define UPPERCASE 2
+#define BLOCK_USAGE 255
+
+void tolowerString(char* target){
+  for(int i = 0; i < strlen(target); i++)
+    if(target[i] >= 'A' && target[i] <= 'Z')
+      target[i] = tolower(target[i]);
+}
+
+void toupperString(char* target){
+  for(int i = 0; i < strlen(target); i++)
+    if(target[i] >= 'a' && target[i] <= 'z')
+      target[i] = toupper(target[i]);
+}
 
 void print_usage(){
   printf("Usage: [-u | -l] [filename]\n");
 }
 
 void print_file(char *filename, int mode){
-  FILE* f = fopen(filename, "r");
-  if(f == NULL)
-    printf("\n\t----- FILE NOT FOUND -----\n\n");
+  char block[BLOCK_USAGE];
+  int stop = 0;
+  size_t size = 0;
 
-  while(!feof(f)){
-    fread(c, sizeof(char), 1, f);
+  FILE* f = fopen(filename, "r");
+  if(f == NULL){
+    printf("\n\t----- FILE NOT FOUND -----\n\n");
+    return;
+  }
+
+  while(!stop){
+    if((size = fread(block, BLOCK_USAGE, 1, f)) < BLOCK_USAGE)
+      stop = 1;
     
     if(mode == UPPERCASE)
-      printf("%s", toupper(c));
+      toupperString(block);
     else if(mode == LOWERCASE)
-      printf("%s", tolower(c));
-    else
-      printf("%s", c);
+      tolowerString(block);
+
+    puts(block);
   }
 
   fclose(f);
@@ -36,13 +56,13 @@ void parse(int argc, char** argv){
       break;
 
     case 2:
-      print_file(argv[1], READ_ONLY)
+      print_file(argv[1], READ_ONLY);
 
     case 3:
       if(!strcmp(argv[1], "-u"))
-        print_file(argv[1], UPPERCASE);
-      else if(!strcmp(argv[1], "-v"))
-        print_file(argv[1], LOWECASE);
+        print_file(argv[2], UPPERCASE);
+      else if(!strcmp(argv[1], "-l"))
+        print_file(argv[2], LOWERCASE);
       else
         print_usage();
   }
