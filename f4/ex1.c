f66cef33a717a38d9ad250431772bf4b24bee30b
Nuno Jesus <up201905477@edu.fe.up.pt>
2022-04-02 18:05:03 +0000

    Added the implementation for exercise 1-

--- f4/ex1.c
+++ f4/ex1.c
@@ -12,29 +12,7 @@ int next_block_size(int count, int buffer_size){
   return (count >= buffer_size) ? buffer_size : count % buffer_size;
 }
 
-int main(int argc, char *argv[]){
-  /* check if exactly one argument is present */
-  if (argc != 2){
-    printf("usage: cat filename\n");
-    return EXIT_FAILURE;
-  }
-
-  /* check if file can be opened and is readable */
-  int fd = open(argv[1], O_RDONLY);
-  if (fd == -1){
-    printf("error: cannot open %s\n", argv[1]);
-    return EXIT_FAILURE;
-  }
-
-  /* get the file size */
-  struct stat info;
-  int ret = lstat(argv[1], &info);
-  if (ret == -1){
-    printf("error: cannot stat %s\n", argv[1]);
-    return EXIT_FAILURE;
-  }
-
-  /* print the contents in blocks */
+void print_file(int fd, struct stat info){
   int count = info.st_size;
   char buffer[BUFFER_SIZE];
   while (count != 0){
@@ -42,8 +20,56 @@ int main(int argc, char *argv[]){
     count -= bytesin;
     write(STDOUT_FILENO, buffer, bytesin);
   }
+}
+
+int open_file(char* filename){
+  int fd = open(filename, O_RDONLY);
+  if (fd == -1){
+    printf("error: cannot open %s\n", filename);
+    exit(-1);
+  }
+
+  return fd;
+}
+
+struct stat read_status(char* filename){
+  struct stat info;
   
-  /* close file */
-  close(fd);
+  if (lstat(filename, &info) == -1){
+    printf("error: cannot stat %s\n", filename);
+    exit(-1);
+  }
+
+  return info;
+}
+
+int main(int argc, char *argv[]){
+  /* check if exactly one argument is present */
+  if (argc < 2){
+    printf("usage: cat [filename]+\n");
+    return EXIT_FAILURE;
+  }
+
+  for(int i = 1; i < argc; i++){
+    /* check if the file exists and is can be read */
+    int fd = open_file(argv[i]);
+    
+    /* get the file size */
+    struct stat info = read_status(argv[i]);
+    
+    /* print the contents in blocks */
+    print_file(fd, info);
+    
+    /* close file */
+    close(fd);
+
+    printf("\n");
+  }
+
+  
+
+
+  
+  
   return EXIT_SUCCESS;
 }
